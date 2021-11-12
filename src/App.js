2fde12296c6e7c7abca1972a5d0591235248076d
noordseoostende <voropayev1@mail.ru>
2021-11-12 13:59:54 +0100

    opnieuw aanmelden

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import ProductScreen from './screens/ProductScreen'
 import CartScreen from './screens/CartScreen'
 import LoginScreen from './screens/LoginScreen'
 import RegisterScreen from './screens/RegisterScreen'
+import ProfileScreen from './screens/ProfileScreen'
 
 
 function App() {
@@ -23,6 +24,7 @@ function App() {
         <Route path='/' component={HomeScreen} exact />
         <Route path='/login' component={LoginScreen} />
         <Route path='/register' component={RegisterScreen} />
+        <Route path='/profile' component={ProfileScreen} />
         <Route path='/product/:id' component={ProductScreen} />
         <Route path='/cart/:id?' component={CartScreen} />
       
