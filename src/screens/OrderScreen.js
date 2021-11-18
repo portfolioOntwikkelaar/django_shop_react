bbaffb3e718f6ed2a3219a3d009659482a824995
noordseoostende <voropayev1@mail.ru>
2021-11-18 22:47:26 +0100

    pay reducer

--- src/screens/OrderScreen.js
+++ src/screens/OrderScreen.js
@@ -30,8 +30,8 @@ function OrderScreen({ match }) {
       
       dispatch(getOrderDetails(orderId))
     }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [order, orderId])
+
+  }, [dispatch, order, orderId])
 
 
 
