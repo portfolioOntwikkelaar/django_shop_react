1b5bd5a91b50a422d3b711d35802cf0efca3437c
noordseoostende <voropayev1@mail.ru>
2021-11-19 22:49:26 +0100

    de gebruikers tabel

---
+++ src/screens/UserListScreen.js
@@ -0,0 +1,27 @@
+import React, {useState, useEffect} from 'react'
+import { LinkContainer } from 'react-router-bootstrap'
+import { Table, Button } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
+import { listUsers } from '../actions/userActions'
+
+function UserListScreen() {
+  const dispatch = useDispatch()
+
+  const userList = useSelector(state => state.userList)
+  const {loading, error, users} = userList
+
+
+  useEffect(() => {
+    dispatch(listUsers())
+  }, [dispatch])
+  return (
+    <div>
+      <h1>Users</h1>
+      
+    </div>
+  )
+}
+
+export default UserListScreen
